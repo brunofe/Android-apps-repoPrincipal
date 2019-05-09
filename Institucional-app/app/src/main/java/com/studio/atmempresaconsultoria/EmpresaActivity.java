b9a433f322e5fa3e6fac3c8a31417bf5ec5d2503
brunofe <ferraresibruno@hotmail.com>
2019-05-09 11:59:18 -0300

    sav

--- Institucional-app/app/src/main/java/com/studio/atmempresaconsultoria/EmpresaActivity.java
+++ Institucional-app/app/src/main/java/com/studio/atmempresaconsultoria/EmpresaActivity.java
@@ -1,13 +1,18 @@
 package com.studio.atmempresaconsultoria;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
-public class EmpresaActivity extends AppCompatActivity {
+public class EmpresaActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empresa);
+        TextView sobreEmpresa = (TextView) findViewById(R.id.texto_empresaId);
+        sobreEmpresa.setMovementMethod(new ScrollingMovementMethod());
     }
 }
--- Institucional-app/app/src/main/java/com/studio/atmempresaconsultoria/MainActivity.java
+++ Institucional-app/app/src/main/java/com/studio/atmempresaconsultoria/MainActivity.java
@@ -1,13 +1,25 @@
 package com.studio.atmempresaconsultoria;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+    private ImageView botaoEmpresa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoEmpresa = findViewById(R.id.empresa_id);
+        botaoEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EmpresaActivity.class));
+            }
+        });
     }
 }
