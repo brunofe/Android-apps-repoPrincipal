a5e77c86885d7b1190cbfc6b58e5e9d9bc1ddc1f
brunofe <ferraresibruno@hotmail.com>
2019-05-10 11:17:54 -0300

    sav

--- Institucional-app/app/src/main/java/com/studio/atmempresaconsultoria/ServicoActivity.java
+++ Institucional-app/app/src/main/java/com/studio/atmempresaconsultoria/ServicoActivity.java
@@ -1,16 +1,13 @@
 package com.studio.atmempresaconsultoria;
 
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.widget.TextView;
 
-public class EmpresaActivity extends Activity {
+public class ServicoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_empresa);
+        setContentView(R.layout.activity_servico);
     }
 }
