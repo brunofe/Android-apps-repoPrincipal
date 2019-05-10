848ca044ffadad3358e595bddee2003b4611690a
brunofe <ferraresibruno@hotmail.com>
2019-05-10 11:57:29 -0300

    sav

--- Institucional-app/app/src/main/java/com/studio/atmempresaconsultoria/MainActivity.java
+++ Institucional-app/app/src/main/java/com/studio/atmempresaconsultoria/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.ImageView;
 
 public class MainActivity extends Activity {
     private ImageView botaoEmpresa;
+    private ImageView botaoServico;
+    private ImageView botaoCliente;
+    private ImageView botaoContato;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +18,32 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         botaoEmpresa = findViewById(R.id.empresa_id);
+        botaoServico = findViewById(R.id.servicos_id);
+        botaoCliente = findViewById(R.id.clientes_id);
+        botaoContato = findViewById(R.id.contato_id);
+
         botaoEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EmpresaActivity.class));
+            }
+        });
+
+        botaoServico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EmpresaActivity.class));
+            }
+        });
+
+        botaoCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EmpresaActivity.class));
+            }
+        });
+
+        botaoContato.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, EmpresaActivity.class));
