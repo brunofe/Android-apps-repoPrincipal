301a262741ca2864deaee3c05fdf963194d0d2fd
brunofe <ferraresibruno@hotmail.com>
2019-05-13 15:41:46 -0300

    sav

--- signosApp/app/src/main/java/com/studio/signos/MainActivity.java
+++ signosApp/app/src/main/java/com/studio/signos/MainActivity.java
@@ -1,13 +1,37 @@
 package com.studio.signos;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    private ListView listaSignos;
+    private String[] signos = {
+            "Áries", "Touro", "Gêmeos", "Câncer", "Leão", "Virgem",
+            "Libra", "Escorpião", "Sagitário", "Capricornio", "Aquário",
+            "Peixes",
+    };
+
+    private String[] perfis = {
+            "Áries perfil", "Touro perfil", "Gêmeos perfil", "Câncer perfil", "Leão perfil", "Virgem perfil",
+            "Libra perfil", "Escorpião perfil", "Sagitário perfil", "Capricornio perfil", "Aquário perfil",
+            "Peixes perfil",
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listaSignos = (ListView) findViewById(R.id.listViewId);
+        ArrayAdapter<String> adaptador = new ArrayAdapter <String> (
+            getApplicationContext(),
+            android.R.layout.simple_list_item_1,
+            android.R.id.text1,
+            signos
+        );
+        listaSignos.setAdapter( adaptador );
     }
 }
