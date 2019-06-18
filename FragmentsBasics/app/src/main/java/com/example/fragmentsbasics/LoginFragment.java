50409cd51c120577bfe716e0f3647417a5f5a21a
brunofe <ferraresibruno@hotmail.com>
2019-06-18 14:51:25 -0300

    sav

--- FragmentsBasics/app/src/main/java/com/example/fragmentsbasics/LoginFragment.java
+++ FragmentsBasics/app/src/main/java/com/example/fragmentsbasics/LoginFragment.java
@@ -6,12 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
+
+//se eu quiser criar manualmente um Fragment eu pos criar uma classe e extender fragment
 public class LoginFragment extends Fragment {
+
+    private TextView texto;
 
     //6:33 minutos
     public LoginFragment() {
@@ -20,10 +25,17 @@ public class LoginFragment extends Fragment {
 
 
     @Override
+    //converte esse arquivo xml em uma view e o android consegue exibir essa tela: "R.layout.fragment_login"
+    //esse fragmento vai ficar dentro desse container: "ViewGroup container"
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        texto = (TextView) view.findViewById(R.id.tv_login);
+        texto.setText("Tela de Login alterada");
+
+        return view;
     }
 
 }
