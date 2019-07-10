c552acf634be0756daadec5b7445f420f7b60d68
brunofe <ferraresibruno@hotmail.com>
2019-07-10 13:41:59 -0300

    sav

--- FlappyBirdClone/core/src/com/game/flappybird/FlappyBird.java
+++ FlappyBirdClone/core/src/com/game/flappybird/FlappyBird.java
@@ -7,27 +7,70 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBird extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private SpriteBatch batch;
+	private Texture[] passaros;
+	private Texture fundo;
+
+	//Atributos de configuração
+	private int laguraDispositivo;
+	private int alturaDispositivo;
+
+	private float variacao=0;
+	private float velocidadeQueda=0;
+	private float posicaoInicialVertical;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		passaros = new Texture[3];
+		passaros[0] = new Texture("passaro1.png");
+        passaros[1] = new Texture("passaro2.png");
+        passaros[2] = new Texture("passaro3.png");
+
+		fundo = new Texture("fundo.png");
+
+		laguraDispositivo= Gdx.graphics.getWidth();
+		alturaDispositivo=Gdx.graphics.getHeight();
+		posicaoInicialVertical=alturaDispositivo/2;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+	    variacao += Gdx.graphics.getDeltaTime()*5;
+	    Gdx.app.log("Variacao","Variacao"+Gdx.graphics.getDeltaTime());
+		velocidadeQueda++;
+
+	    if(variacao > 2){
+	        variacao = 0;
+        }
+
+	    if(posicaoInicialVertical>0){
+			posicaoInicialVertical -= velocidadeQueda;
+		}
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		batch.draw(fundo, 0,0, laguraDispositivo, alturaDispositivo);
+		batch.draw(passaros[(int)variacao], 30, posicaoInicialVertical);
+
 		batch.end();
 	}
-	
+
+	/*
+	 * O método dispose () do ApplicationListener é o método
+	 * do ciclo de vida e chamado quando o aplicativo é destruído.
+	 * Quaisquer recursos descartáveis que você criou no método create ()
+	 * devem ser destruídos nesse método.
+	  * */
 	@Override
 	public void dispose () {
+		//batch.dispose();
+		//img.dispose();
 		batch.dispose();
-		img.dispose();
+		passaros[0].dispose();
+        passaros[1].dispose();
+        passaros[2].dispose();
+		fundo.dispose();
+
 	}
 }
